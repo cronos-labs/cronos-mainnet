a1e4d98b6228fac58813b925a54dcbe9e81557cd
kai zhang <kai.zhang@crypto.com>
2023-01-13 12:36:34 +0800

    Change to curl LJ

--- 1-click/1click-reconfig.sh
+++ 1-click/1click-reconfig.sh
@@ -5,7 +5,7 @@ set -e
 download_genesis()
 {
     echo_s "💾 Downloading $NETWORK genesis"
-    sudo curl -sS $NETWORK_URL/$NETWORK/genesis.json -o $CM_GENESIS
+    sudo curl -LJ $NETWORK_URL/$NETWORK/genesis.json -o $CM_GENESIS
 }
 shopt -s globstar
 download_binary()
